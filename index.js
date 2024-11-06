a446bf2007720332268cfbd8c950c76ba43ca3d5
Rakib <rh.rakibhasan365@gmail.com>
2024-11-06 21:40:33 +0600

    user information post related api create

--- index.js
+++ index.js
@@ -28,9 +28,27 @@ async function run() {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
+    const usersCollection = client.db('halalHavenDB').collection('users');
     const menuCollection = client.db('halalHavenDB').collection('menu');
     const reviewCollection = client.db('halalHavenDB').collection('reviews');
+    const cartCollection = client.db('halalHavenDB').collection('carts');
 
+    // Users related api
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Already exist", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().insertOne();
+      res.send(result);
+    })
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -40,8 +58,29 @@ async function run() {
     app.get('/review', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
-    })
+    });
 
+    // order cart collection related
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+  });
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new Object(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
