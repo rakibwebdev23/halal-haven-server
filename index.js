9db60f7f78383a6158750c9f0ab6dd2b5f43a6de
Rakib <rh.rakibhasan365@gmail.com>
2024-11-11 01:42:59 +0600

    create payment intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId, Admin } = require('mongodb');
 
@@ -186,7 +187,24 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
+
+    // payment gateway related api 
+    // payment intent 
+    app.post('/create-payment-intent', verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log('amount inside the intent', amount);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+      });
+      res.send({
+          clientSecret: paymentIntent.client_secret
+      });
+  });
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
